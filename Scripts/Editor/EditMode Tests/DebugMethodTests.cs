f07746f0d8d45b915231a606bf5c17c8d820b49a
Sandro Figo <sandro.figo@gmx.at>
2020-06-30 11:09:41 +0200

    Add unit test

--- Scripts/Editor/EditMode Tests/DebugMethodTests.cs
+++ Scripts/Editor/EditMode Tests/DebugMethodTests.cs
@@ -29,5 +29,37 @@ namespace DebugMenu.Tests
             Assert.IsTrue(method1.HasParameters);
             Assert.IsTrue(method2.HasParameters);
         }
+
+        [Test]
+        public void FindNodeByPathWorks()
+        {
+            Node root = new Node
+            {
+                name = "Root"
+            };
+
+            Node sub1 = new Node
+            {
+                name = "Sub1"
+            };
+
+            Node sub2 = new Node
+            {
+                name = "Sub2"
+            };
+
+            root.children.Add(sub1);
+            sub1.children.Add(sub2);
+
+            DebugMenuManager.AutomaticCreation = true;
+
+            DebugMenuManager.Instance.nodes.Add(root);
+
+            Node result1 = DebugMenuManager.Instance.FindNodeByPath("Root.Sub1.Sub2");
+            Assert.IsNotNull(result1);
+            
+            Node result2 = DebugMenuManager.Instance.FindNodeByPath("Root.Sub1.Sub321");
+            Assert.IsNull(result2);
+        }
     }
 }
